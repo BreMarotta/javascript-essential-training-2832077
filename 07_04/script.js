08a8ba313c18d96a6ea8aeb5a6f29e3a4ccad1b9
Breanne <BreMarotta@gmail.com>
2023-01-13 11:34:13 -0500

    solvle challenge

--- 07_04/script.js
+++ 07_04/script.js
@@ -9,3 +9,26 @@
  */
 
 
+let list = ["lip gloss", "hair ties", "coffee", "lotion", "nail polish", "lego figures", "headphones", "clock"]
+
+// array is 8 itmes:
+console.log("array length", list.length)
+
+// remove last itme
+list.pop()
+console.log("remove last", list)
+
+// dd the last item as the first item on the array 
+list.unshift("eraser")
+console.log("add eraser to beginning", list)
+
+// Sort the items by alphabetical order
+list.sort();
+console.log("sorted alphabetically", list)
+
+// Use the find() method to find a specific item in the array
+let x = list.find(x => x === "lip gloss")
+console.log(x)
+
+let newList = list.filter(y => y !== x)
+console.log(newList)
